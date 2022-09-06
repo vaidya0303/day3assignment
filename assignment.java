9dbcd2d9ae3643969bd7107997435ca364e1ce3e
saurabh vaidya <vaidya0304@gmail.com>
2022-09-06 18:54:19 +0530

    second

--- assignment.java
+++ assignment.java
@@ -1,17 +1,27 @@
-public class assignment {
-public static void main(String[] args) {
 
-// Constants 
-int IS_FULL_TIME = 1; 
-// Computation
+public class assignment { 
+    public static void main(String[] args) {
 
-double empCheck = Math.floor(Math.random() * 10) % 2;
- if (empCheck == IS_FULL_TIME)
-System.out.println("Employee is Present");
-else
-
-System.out.println("Employee is Absent");
-
+    // Constants
+     int IS_FULL_TIME = 1;
+    int EMP_RATE_PER_HOUR = 20;
+    
+    // Variables
+     int empHrs = 0;
+    
+    int empWage = 0;
+    
+    // Computation
+     double empCheck = Math.floor(Math.random() * 10) % 2;
+      if (empCheck == IS_FULL_TIME) 
+          empHrs = 8;
+      else
+          empHrs = 0; 
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp Wage: "+ empWage);
+    
+    }
+    
 }
-
-}
+    
+    
