340c3e4cecb9ac80ecc6c35cc0644869651ac7cc
saurabh vaidya <vaidya0304@gmail.com>
2022-09-06 19:32:09 +0530

    fourth

--- assignment.java
+++ assignment.java
@@ -1,37 +1,38 @@
-public class assignment {
+public class assignment { 
 
-    public static void main(String[] args) {
-    
-    // Constants
-     int IS_PART_TIME = 1;
-     int IS_FULL_TIME = 2;
-     int EMP_RATE_PER_HOUR =20;
-    
-     // Variables
-    
-    int empHrs = 0;
-    int empWage = 0; 
-    // Computation
-    double empCheck = Math.floor(Math.random() * 10) % 3;
-     if (empCheck == IS_PART_TIME)
-        empHrs = 4;
-    else if (empCheck == IS_FULL_TIME)
-        empHrs = 8;
-    else
-        empHrs = 0;
-    
-     empWage = empHrs  * EMP_RATE_PER_HOUR; 
-     System.out.println("Emp Wage: " + empWage);
-    }
-  
-    
+public static final int IS_PART_TIME = 1;
+ public static final int IS_FULL_TIME = 2; 
+ public static final int EMP_RATE_PER_HOUR = 20;
+
+public static void main(String[] args) {
+
+// Variables 
+int empHrs = 0;
+
+int empWage = 0;
+
+// Computation
+ int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+
+switch (empCheck) {
+
+case IS_PART_TIME: 
+   empHrs = 4;
+
+   break; 
+case IS_FULL_TIME:
+   empHrs = 8;
+
+   break;
+   
+  default: 
+   empHrs = 0;
 }
-    
-    
-    
-    
-    
-    
-    
-    
-    
+   empWage = empHrs * EMP_RATE_PER_HOUR;
+   System.out.println ("Emp Wage:" + empWage);
+
+  }
+}
+
+
+
