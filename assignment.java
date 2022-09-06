9c26c4d2a99e5b54c73f0f8080fe30f0a5dbc214
saurabh vaidya <vaidya0304@gmail.com>
2022-09-06 19:15:40 +0530

    third

--- assignment.java
+++ assignment.java
@@ -1,27 +1,37 @@
+public class assignment {
 
-public class assignment { 
     public static void main(String[] args) {
-
+    
     // Constants
-     int IS_FULL_TIME = 1;
-    int EMP_RATE_PER_HOUR = 20;
+     int IS_PART_TIME = 1;
+     int IS_FULL_TIME = 2;
+     int EMP_RATE_PER_HOUR =20;
     
-    // Variables
-     int empHrs = 0;
+     // Variables
     
-    int empWage = 0;
-    
+    int empHrs = 0;
+    int empWage = 0; 
     // Computation
-     double empCheck = Math.floor(Math.random() * 10) % 2;
-      if (empCheck == IS_FULL_TIME) 
-          empHrs = 8;
-      else
-          empHrs = 0; 
-        empWage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Emp Wage: "+ empWage);
+    double empCheck = Math.floor(Math.random() * 10) % 3;
+     if (empCheck == IS_PART_TIME)
+        empHrs = 4;
+    else if (empCheck == IS_FULL_TIME)
+        empHrs = 8;
+    else
+        empHrs = 0;
     
+     empWage = empHrs  * EMP_RATE_PER_HOUR; 
+     System.out.println("Emp Wage: " + empWage);
     }
+  
     
 }
+    
+    
+    
+    
+    
+    
+    
     
     
