eac2de0e759b8289a8445b662e7646b2af70fea4
saurabh vaidya <vaidya0304@gmail.com>
2022-09-06 20:28:45 +0530

    sixth

--- assignment.java
+++ assignment.java
@@ -1,45 +1,48 @@
 public class assignment {
 
-    public static final int IS_PART_TIME = 1;
-    public static final int EMP_RATE_PER_HOUR = 20;
-    public static final int IS_FULL_TIME = 2;
-    public static final int NUM_OF_WORKING_DAYS = 2;
-    
-
-    public static void main(String[] args) { 
-        // Variables
-    
-        int empHrs= 0; 
-         int   empWage = 0;
-            int totalEmpWage = 0; 
-             // Computation 
-             for (int day = 0; day< NUM_OF_WORKING_DAYS; day++) { 
-                int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-    
-                switch (empCheck) {
-                
-                 case IS_PART_TIME: 
-                       empHrs = 4;
-                
-                 break; 
-                     case IS_FULL_TIME: 
-                          empHrs = 8;
-                
-                 break; 
-                 default: 
-                 empHrs = 0;
-
-                }
+public static final int IS_PART_TIME= 1; 
+public static final int IS_FULL_TIME = 2;
+public static final int EMP_RATE_PER_HOUR=28; 
+public static final int NUM_OF_WORKING_DAYS=2; 
+public static final int MAX_HRS_IN_MONTH = 10;
 
 
-              empWage = empHrs * EMP_RATE_PER_HOUR;
-              totalEmpWage += empWage; 
-              System.out.println("Emp Wage:" +empWage);
+public static void main (String[] args) {
 
-            }
+   // Variables 
+int empHrs = 0, totalEmpHrs=0, totalworkingDays = 8;
 
-              System.out.println("Total Emp Wage:" + totalEmpWage);
+// Computation
 
-                }
-            }
-                 
+while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
+ totalworkingDays < NUM_OF_WORKING_DAYS) {
+
+totalworkingDays++;
+
+int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+
+switch (empCheck) {
+     case IS_PART_TIME:
+
+  empHrs = 4;
+     break;
+
+     case IS_FULL_TIME:
+         empHrs =8;
+
+     break;
+     default: 
+     empHrs = 0;
+}
+
+     totalEmpHrs += empHrs;
+
+     System.out.println("Day#:" + totalworkingDays + "Emp Hr: " +empHrs); 
+}
+     int totalEmpwage = totalEmpHrs * EMP_RATE_PER_HOUR;
+
+     System.out.println("Total Emp Wage: "+totalEmpwage);
+
+    }
+ }
+
