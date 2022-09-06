59800be0ac396b78561fe02c172724a692401ca4
saurabh vaidya <vaidya0304@gmail.com>
2022-09-06 18:28:36 +0530

    first

---
+++ assignment.java
@@ -0,0 +1,17 @@
+public class assignment {
+public static void main(String[] args) {
+
+// Constants 
+int IS_FULL_TIME = 1; 
+// Computation
+
+double empCheck = Math.floor(Math.random() * 10) % 2;
+ if (empCheck == IS_FULL_TIME)
+System.out.println("Employee is Present");
+else
+
+System.out.println("Employee is Absent");
+
+}
+
+}
